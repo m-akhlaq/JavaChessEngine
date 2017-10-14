051d1e2f3fff23b92ebcd3ddbf1dfd8dfb4fda2f
Muhammad Shaheer Akhlaq <msa172@scarletmail.rutgers.edu>
2017-10-13 22:12:46 -0400

    added method renderBoard that renders the board

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -6,14 +6,28 @@ public class Chess {
 	public static void main(String[] args) {
 		
 		Pieces board[][]=new Pieces[8][8];
-		
+		renderBoard(board);
 	}
 	
 	
 	
-	public void renderBoard(Pieces [][] b){
-		
-		
+	public static void renderBoard(Pieces [][] b){
+		int color=2;
+		for (int x=0;x<b.length;x++){
+			for (int y=0;y<b[x].length;y++){
+				if (b[x][y]==null){
+					if (color%2==0){
+						System.out.print("   ");
+					}else
+						System.out.print("##");
+				}
+				color++;
+			}
+			
+			System.out.println(" "+(8-x));
+			color+=1;
+		}
+		System.out.println("a  b c  d  e f   g  h");
 		
 	}
 
