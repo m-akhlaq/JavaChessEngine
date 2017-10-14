d2a76c2170f1402ee79a6ad211941eca50416204
Muhammad Shaheer Akhlaq <msa172@scarletmail.rutgers.edu>
2017-10-13 21:50:48 -0400

    added package pieces and abstract class Pieces

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,10 +1,20 @@
 package chess;
+import pieces.Pieces;
 
 public class Chess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Pieces board[][]=new Pieces[8][8];
+		
+	}
+	
+	
+	
+	public void renderBoard(Pieces [][] b){
+		
+		
+		
 	}
 
 }
