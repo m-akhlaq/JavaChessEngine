213a45ec9bb3b86b7570f05ad71fb5400b01b136
Your Name <jhb141@scarletmail.rutgers.edu>
2017-10-18 11:56:00 -0400

    Rook class with four while loops going up, down, left, and right

---
+++ src/pieces/Rook.java
@@ -0,0 +1,129 @@
+package pieces;
+import java.util.ArrayList;
+import java.util.List;
+
+import utilities.Coordinates;
+
+public class Rook extends Pieces{
+	
+	public Rook(int team, int row,int column, String name) {
+		super(team, row,column, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean canMove(Pieces[][] board, int r,int c) {
+		
+		if (r>7 || c>7 || r<0 || c<0)
+			return false;
+		
+		Coordinates proposedMove = new Coordinates(r,c);
+		List<Coordinates> validMoves = new ArrayList<Coordinates>();
+		//defining valid moves for both Rooks
+			
+		//moving up
+		//System.out.println("MOVING UP");
+		int tempRow=row-1;//used to track the distance from the current position
+		while(tempRow>=0){
+			if(board[tempRow][column]==null){//position is open
+				validMoves.add(new Coordinates(tempRow,column));
+			}
+			else if(board[tempRow][column].getTeam()==team){//same team
+				break;
+			}
+			else if(board[tempRow][column].getTeam()!=team){//other team
+				validMoves.add(new Coordinates(tempRow,column));//replaces other team's piece
+				break;//cant move further
+			}
+			tempRow--;//move up a row
+		}
+		
+		//moving down
+		//System.out.println("MOVING DOWN");
+		int tempRow2=row+1;//used to track the distance from the current position
+		while(tempRow2<=7){
+			if(board[tempRow2][column]==null){//position is open
+				validMoves.add(new Coordinates(tempRow2,column));
+			}
+			else if(board[tempRow2][column].getTeam()==team){//same team
+				break;
+			}
+			else if(board[tempRow2][column].getTeam()!=team){//other team
+				validMoves.add(new Coordinates(tempRow2,column));//replaces other team's piece
+				break;//cant move further
+			}
+			tempRow2++;//move up a row
+		}
+		
+		//moving right
+		//System.out.println("MOVING RIGHT");
+		int tempColumn=column+1;//used to track the distance from the current position
+		while(tempColumn<=7){
+			if(board[row][tempColumn]==null){//position is open
+				validMoves.add(new Coordinates(row, tempColumn));
+			}
+			else if(board[row][tempColumn].getTeam()==team){//same team
+				break;
+			}
+			else if(board[row][tempColumn].getTeam()!=team){//other team
+				validMoves.add(new Coordinates(row,tempColumn));//replaces other team's piece
+				break;//cant move further
+			}
+			tempColumn++;//move right one
+		}
+			
+		//moving left
+		//System.out.println("MOVING LEFT");
+		int tempColumn2=column-1;//used to track the distance from the current position
+		while(tempColumn2>=0){
+			if(board[row][tempColumn2]==null){//position is open
+				validMoves.add(new Coordinates(row, tempColumn2));
+			}
+			else if(board[row][tempColumn2].getTeam()==team){//same team
+				break;
+			}
+			else if(board[row][tempColumn2].getTeam()!=team){//other team
+				validMoves.add(new Coordinates(row, tempColumn2));//replaces other team's piece
+				break;//cant move further
+			}
+			tempColumn2--;//move left one
+		}
+			
+			
+		
+		
+		System.out.println(validMoves);
+				
+		return false;
+		
+	}
+	
+	//method to check which moves are valid
+	public List<Coordinates> checkMoves(Coordinates[] moves, Pieces[][] board){
+		List<Coordinates> validMoves = new ArrayList<Coordinates>();
+		for(int i=0; i<moves.length;i++){
+			int rowTemp=moves[i].getRow();
+			int columnTemp = moves[i].getColumn();
+			if(board[rowTemp][columnTemp]==null){
+				validMoves.add(moves[i]);
+			}
+		}
+		return validMoves;
+		}
+
+	@Override
+	public String getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public int getTeam(){
+		return team;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+}
